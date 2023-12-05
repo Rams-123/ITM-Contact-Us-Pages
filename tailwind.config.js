c180040e18ae445e44ee9428c118aa49ab595066
Rams-123 <rams@letsupgrade.in>
2023-12-05 19:20:55 +0530

    contact-us-pages-changes

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.html",
+    "./public/style.css",
+    "./public/**/*.js",
+    "./public/assets/",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
